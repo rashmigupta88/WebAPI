269b1252fa0c9df8e28ce8e3867f69e071ab4a7e
bloggernest <44660748+rashmigupta88@users.noreply.github.com>
2022-10-09 01:20:29 -0400

    WebAPI

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55854/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("values");
+                //var responseTask = client.PutAsync("values/5", null);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    var students = readTask.Result;
+
+                    Console.WriteLine(students.ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApi1/WebApi1/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+
+            using (SqlConnection conn = new SqlConnection("Data Source=sc-y-db-bms-1;Initial Catalog=OMSDEV3;Integrated Security=true;Connection Timeout=60"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO temp.DBTest (LoginDateTime) VALUES (GETDATE())", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                }
+            }
+            return new string[] { "Success", "Login Successfully" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post(string value)
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=sc-y-db-bms-1;Initial Catalog=OMSDEV3;Integrated Security=true;Connection Timeout=60"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO temp.DBTest (LoginDateTime) VALUES (GETDATE())", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                }
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
